3e73ebb1c71b196919f853868073a515abb38d38
IvanMagda <ivanmagda164@gmail.com>
2024-04-07 15:00:40 +0300

    Initial Commit

---
+++ SwiftUIAnimationLearning/SwiftUIAnimationLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationLearningApp.swift
+//  SwiftUIAnimationLearning
+//
+//  Created by Ivan Magda on 07.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
